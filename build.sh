6f40ef8e70438a65a5b03eb486ce86dd7b18b367
yuji-morita <yuji-morita@united.jp>
2019-12-27 10:37:01 +0900

    build

--- build.sh
+++ build.sh
@@ -6,18 +6,15 @@ cd build
 git clone https://github.com/yuji-morita/android-doc-test.git && cd $(basename $_ .git)
 tags=$(git tag)
 
-docker run --rm -it -v ${PWD}:/docs squidfunk/mkdocs-material build -d docs
+docker run --rm -it -v ${PWD}:/docs squidfunk/mkdocs-material build -d build/docs
 
 for tag in ${tags[@]}
 do
-    git checkout . > /dev/null 2>&1
-    git clean -df . > /dev/null 2>&1
     git checkout refs/tags/$tag > /dev/null 2>&1
-
-    docker run --rm -it -v ${PWD}:/docs squidfunk/mkdocs-material build -d docs/$tag
+    docker run --rm -it -v ${PWD}:/docs squidfunk/mkdocs-material build -d build/docs/$tag
 done
 
 rm -rf ../../docs
-cp -r docs ../../
+cp -r build/docs ../../
 
 rm -rf ../../build
